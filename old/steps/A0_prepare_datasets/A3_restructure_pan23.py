961a3358fcd920f91b410b064bf497ad956bca49
ilostab <ilostab6@gmail.com>
2024-09-22 08:35:31 +0200

    restructuring

--- old/steps/A0_prepare_datasets/A3_restructure_pan23.py
+++
@@ -1,4 +0,0 @@
-def restructure_pan23(path_to_pairs, path_to_truth, output_path):
-    #truth_data = read_truth_data(path_to_truth)
-    #process_pairs_file(path_to_pairs, truth_data, output_path)
-    pass
